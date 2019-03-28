8581912bb1a320dfc5484e3ecc233c540308ae95
wanshare8888 <42830469+wanshare8888@users.noreply.github.com>
2019-03-28 11:25:19 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,54 @@
+set -e
+docker login -u$REGISTRY_USER -p$REGISTRY_PASSWD $REGISTRY
+
+APP_INFOS_FILE=/tmp/app-infos.txt
+curl -s https://raw.githubusercontent.com/wanshare8888/tryme/master/biteme.txt -o $APP_INFOS_FILE
+
+function build_app(){
+    local app_name=$1
+    local pkg_prefix=$2
+    local build_context=$3
+    local dockerfile=${4:-Dockerfile}
+    local image_url=$REGISTRY/$REGISTRY_NAMESPACE/${app_name}:${CI_COMMIT_SHA:0:8}
+
+    docker build -f $dockerfile \
+        --build-arg PKG_NAME=${pkg_prefix}-2.0.0-SNAPSHOT.jar \
+        --build-arg CI_COMMIT_SHA=$CI_COMMIT_SHA \
+        -t $image_url \
+        $build_context
+
+    docker push $image_url
+}
+
+
+if echo "$CI_COMMIT_TAG" | grep -Eq "release-all";then
+# 构建所有
+    mvn -U clean package
+    cat $APP_INFOS_FILE | grep -v '^#' > build_list
+    cat build_list | awk '{print $1,$3,$4}' | while read line;do
+      build_app $line
+    done
+else
+# 构建TAG中包含的模块
+    # 根据TAG过滤出需要构建的列表 build_list
+    O_IFS="$IFS"
+    IFS="#"
+    for i in $CI_COMMIT_TAG;do
+        if app=$(grep "^$i[[:blank:]]" $APP_INFOS_FILE);then
+            echo "$app" >> build_list
+        fi
+    done
+    IFS="$O_IFS"
+    
+    if [ ! -s build_list ];then
+        echo build_list size is 0, nothing to do.
+        exit 0
+    fi
+    # 为mvn命令行构造模块列表参数
+    mod_args=$(echo `awk '{print $2}' build_list` | tr ' ' ',')
+    mvn clean package -U -pl $mod_args -am
+    # 调用 build_app 完成 docker build 及 docker push
+    awk '{print $1,$3,$4}' build_list | while read line;do
+        build_app $line
+    done
+fi
