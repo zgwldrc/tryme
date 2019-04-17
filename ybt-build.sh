43eb0fbec4289fb6267b06b9a6445430ae562959
wanshare8888 <42830469+wanshare8888@users.noreply.github.com>
2019-04-17 21:42:21 +0800

    Update ybt-build.sh

--- ybt-build.sh
+++ ybt-build.sh
@@ -3,7 +3,6 @@
 # 运行环境: zgwldrc/maven-and-docker
 # docker run --rm -it zgwldrc/maven-and-docker sh
 # 该脚本用于ybt项目在gitlab-ci系统中的构建
-set -x
 function include_url_lib() {
   local t="$(mktemp)"
   local url="$1"
