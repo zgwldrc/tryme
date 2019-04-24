9beafa1edfe3e78d5f14b7c77d7f4cc8bbaebd76
夏禹 <xiayu@wanshare.com>
2019-04-24 15:58:26 +0800

    update build script

--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@
 # 运行环境: zgwldrc/maven-and-docker
 # docker run --rm -it zgwldrc/maven-and-docker sh
 # 该脚本用于crush项目在gitlab-ci系统中的构建
+# BUILD_EXCLUDE_LIST
+# DEPLOY_EXCLUDE_LIST
+# REGISTRY
+# REGISTRY_USER
+# REGISTRY_PASSWD
+# REGISTRY_NAMESPACE
+# DOCKERFILE_URL
+# APP_INFOS_URL
+# BUILD_LIST
 set -e
 function include_url_lib() {
   local t="$(mktemp)"
@@ -30,6 +39,7 @@ REGISTRY_PASSWD
 REGISTRY_NAMESPACE
 DOCKERFILE_URL
 APP_INFOS_URL
+BUILD_LIST
 '
 check_env $ENV_CHECK_LIST
 
@@ -38,15 +48,16 @@ docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASSWD" "$REGISTRY"
 
 function build_app(){
     local app_name=$1
-    local pkg_prefix=$2
+    local package_name=$2
     local build_context=$3
-    local dockerfile=${4:-Dockerfile}
-    local image_url=${REGISTRY#http*://}/$REGISTRY_NAMESPACE/${app_name}:${CI_COMMIT_SHA:0:8}
-    docker build -f $dockerfile \
-        --build-arg PKG_NAME=${pkg_prefix}-2.0.0-SNAPSHOT.jar \
+
+    local image_url=$REGISTRY/$REGISTRY_NAMESPACE/${app_name}:${CI_COMMIT_SHA:0:8}
+    docker build -f Dockerfile \
+        --build-arg PKG_NAME=${package_name} \
         -t $image_url \
         $build_context
     docker push $image_url
+    
     if [ "$IMAGE_CLEAN" == "true" ];then
       docker image rm $image_url
     fi
@@ -55,26 +66,25 @@ function build_app(){
 APP_INFOS_FILE=/tmp/app-infos.txt
 curl -s "$APP_INFOS_URL" -o $APP_INFOS_FILE
 curl -s "$DOCKERFILE_URL" -o Dockerfile
+
 BUILD_EXCLUDE_LIST="${BUILD_EXCLUDE_LIST/,/|}"
 DEPLOY_EXCLUDE_LIST="${DEPLOY_EXCLUDE_LIST/,/|}"
-if echo "$CI_COMMIT_REF_NAME" | grep -Eq "release-all|dev" || [ "$BUILD_LIST" == "release-all" ] ;then
+
+if [ "$BUILD_LIST" == "release-all" ] ;then
     # 构建所有
     mvn -U clean package
-    cat $APP_INFOS_FILE | grep -Ev "^#|${BUILD_EXCLUDE_LIST:-NOTHINGTOEXCLUDE}" | tee build_list | grep -Ev "crush-flyway|${DEPLOY_EXCLUDE_LIST:-NOTHINGTOEXCLUDE}" | awk '{print $1}' > deploy_list
-    cat build_list | awk '{print $1,$3,$4}' | while read line;do
-        build_app $line
+    cat $APP_INFOS_FILE | grep -Ev "^#|${BUILD_EXCLUDE_LIST:-NOTHINGTOEXCLUDE}" | tee build_list | grep -Ev "${DEPLOY_EXCLUDE_LIST:-NOTHINGTOEXCLUDE}" | awk '{print $1}' > deploy_list
+    awk '{print $1,$3"-"$4".jar",$2"/target/"}' build_list | while read app_name package_name build_context;do
+        build_app $app_name $package_name $build_context
     done
 else
-    if [ "$CI_COMMIT_REF_NAME" != "master" ];then
-        BUILD_LIST="$CI_COMMIT_REF_NAME"       
-    fi
     # 构建TAG中包含的模块
     # 根据TAG过滤出需要构建的列表 build_list
     O_IFS="$IFS"
-    IFS="#"
-    for i in $BUILD_LIST;do
-        if app=$(grep "^$i[[:blank:]]" $APP_INFOS_FILE);then
-            echo "$app" >> build_list
+    IFS=","
+    for app_name in $BUILD_LIST;do
+        if record=$(grep "^$app_name[[:blank:]]" $APP_INFOS_FILE);then
+            echo "$record" >> build_list
         fi
     done
     IFS="$O_IFS"
@@ -87,8 +97,8 @@ else
     mod_args=$(echo `awk '{print $2}' build_list` | tr ' ' ',')
     mvn clean package -U -pl $mod_args -am
     # 调用 build_app 完成 docker build 及 docker push
-    awk '{print $1,$3,$4}' build_list | while read line;do
-        build_app $line
+    awk '{print $1,$3"-"$4".jar",$2"/target/"}' build_list | while read app_name package_name build_context;do
+        build_app $app_name $package_name $build_context
     done
-    awk '{print $1}' build_list | grep -Ev 'crush-config-server|crush-flyway' > deploy_list
+    awk '{print $1}' build_list | grep -Ev '${DEPLOY_EXCLUDE_LIST:-NOTHINGTOEXCLUDE}' > deploy_list
 fi
