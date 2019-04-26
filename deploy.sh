a0be00005ab4711934f33968403d7dab42e01aee
夏禹 <xiayu@wanshare.com>
2019-04-26 12:38:43 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 # we assume there is a file named deploy_list under current working directory
-# BUILD_EXCLUDE_LIST
-# DEPLOY_EXCLUDE_LIST
+# NAMESPACE
+# KUBECONFIG_CONTENT
 # REGISTRY
-# REGISTRY_USER
-# REGISTRY_PASSWD
 # REGISTRY_NAMESPACE
-# DOCKERFILE_URL
-# APP_INFOS_URL
-# BUILD_LIST
 # ${CI_COMMIT_SHA:0:8}
+function _check_env(){
+    case "" in
+    "$NAMESPACE"|"$KUBECONFIG_CONTENT")
+        echo "must set env: NAMESPACE and KUBECONFIG"
+        exit 1
+        ;;
+    *)
+       ;;
+   esac
+}
+
+function _init_env(){
+    mkdir -p $HOME/.kube/
+    echo -e "$KUBECONFIG_CONTENT" > $HOME/.kube/config
+    kubectl config set-context `kubectl config current-context` --namespace=$NAMESPACE
+}
+
+_check_env
+_init_env
 
 if [ ! -e deploy_list ];then
     echo deploy_list not found 
